b950b9ed6eabad40ac2e0d51e5d5a4ded1663b54
José Victor <josevictor.jvds@gmail.com>
2024-04-21 08:58:30 -0300

    changes: remove arquivos desnecessarios

--- resources/views/_to_copy/pages/supplier/list-buttons.blade.php
+++
@@ -1,4 +0,0 @@
-@php $id = $row->getPrimaryValue(); @endphp
-
-@include($view_prefix . 'components.buttons.edit-button', ['route' => route('supplier.show', $id)])
-@include($view_prefix . 'components.buttons.delete-button', ['route' => route('supplier.destroy', $id)])

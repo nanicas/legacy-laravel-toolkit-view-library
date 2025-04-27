4be79fbbc02e3e41b598990d1348aa375e1c4edd
José Victor <josevictor.jvds@gmail.com>
2025-04-27 12:06:39 -0300

    fix: remove cópia desnecessária do css

--- app/Providers/BootstrapServiceProvider.php
+++ app/Providers/BootstrapServiceProvider.php
@@ -16,19 +16,18 @@ class BootstrapServiceProvider extends ServiceProvider
         $src = __DIR__ . '/../..';
 
         $this->publishes([
-            $src . '/resources/css' => resource_path('vendor/legacy_laravel_toolkit_view_library/css'),
             $src . '/resources/js' => resource_path('vendor/legacy_laravel_toolkit_view_library/js'),
             $src . '/resources/sass' => resource_path('vendor/legacy_laravel_toolkit_view_library/sass'),
             $src . '/resources/vendor' => resource_path('vendor/legacy_laravel_toolkit_view_library/vendor'),
-                ], 'legacy_laravel_toolkit_view_library:resources');
+        ], 'legacy_laravel_toolkit_view_library:resources');
 
         $this->publishes([
             $src . '/resources/views' => resource_path('views/vendor/legacy_laravel_toolkit_view_library'),
-                ], 'legacy_laravel_toolkit_view_library:views');
+        ], 'legacy_laravel_toolkit_view_library:views');
 
         $this->publishes([
             $src . '/public' => public_path('vendor/legacy_laravel_toolkit_view_library')
-                ], 'legacy_laravel_toolkit_view_library:public');
+        ], 'legacy_laravel_toolkit_view_library:public');
 
         $this->loadViewsFrom(
             resource_path('views/vendor/legacy_laravel_toolkit_view_library'),
